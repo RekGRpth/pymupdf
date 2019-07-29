e32c5a7c32f225028bb278a2c3fe218ce72ffc68
RekGRpth <rekgrpth@gmail.com>
2019-07-29 16:35:01 +0500

    up

--- pymupdf.c
+++ pymupdf.c
@@ -37,29 +37,29 @@ PyObject *mupdf(PyObject *data, const char *input_type, const char *output_type,
     if (PyBytes_AsStringAndSize(data, &input_data, &input_len)) goto ret;
     fz_context *ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
     if (!ctx) goto ret;
-    fz_stream *input_stream = NULL;
-    fz_buffer *output_buffer = NULL;// fz_var(output_buffer);
+    fz_stream *stm = NULL;// fz_var(stm);
+    fz_buffer *buf = NULL;// fz_var(buf);
     fz_document *doc = NULL;// fz_var(doc);
     fz_document_writer *wri = NULL;// fz_var(wri);
     fz_try(ctx) {
         fz_register_document_handlers(ctx);
         fz_set_use_document_css(ctx, 1);
-        output_buffer = fz_new_buffer(ctx, 0);
-        fz_set_user_context(ctx, output_buffer);
-        input_stream = fz_open_memory(ctx, (unsigned char *)input_data, input_len);
-        doc = fz_open_document_with_stream(ctx, input_type, input_stream);
+        buf = fz_new_buffer(ctx, 0);
+        fz_set_user_context(ctx, buf);
+        stm = fz_open_memory(ctx, (unsigned char *)input_data, input_len);
+        doc = fz_open_document_with_stream(ctx, input_type, stm);
         wri = fz_new_document_writer(ctx, "buf:", output_type, options);
         runrange(ctx, doc, range, wri);
     } fz_always(ctx) {
         if (wri) fz_close_document_writer(ctx, wri);
         if (wri) fz_drop_document_writer(ctx, wri);
         if (doc) fz_drop_document(ctx, doc);
-        if (input_stream) fz_drop_stream(ctx, input_stream);
+        if (stm) fz_drop_stream(ctx, stm);
     } fz_catch(ctx) {
         goto fz_drop_context;
     }
     unsigned char *output_data = NULL;
-    size_t output_len = fz_buffer_storage(ctx, output_buffer, &output_data);
+    size_t output_len = fz_buffer_storage(ctx, buf, &output_data);
     bytes = PyBytes_FromStringAndSize((const char *)output_data, (Py_ssize_t)output_len);
 fz_drop_context:
     fz_drop_context(ctx);
